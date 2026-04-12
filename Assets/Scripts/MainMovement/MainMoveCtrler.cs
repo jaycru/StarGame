58cfca4c149c1a69995f23af504edc586c8934b6
Jaycr <24371020@buaa.edu.cn>
2026-04-12 09:14:26 +0800

    backup

---
+++ Assets/Scripts/MainMovement/MainMoveCtrler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//*****************************************
+//创建人： Jaycr 
+//功能说明：
+//***************************************** 
+public class MainMoveCtrler : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    } 
+}
