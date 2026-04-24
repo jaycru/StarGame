0d209ae4aa0f533f12db16fde5684fb4f8a272ef
Jaycr <24371020@buaa.edu.cn>
2026-04-24 19:44:26 +0800

    4/24 19:44

---
+++ Assets/Scripts/Manager/GunManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//*************************
+//创建人：Jaycr
+//创建时间：#CreateTime#
+//描述：
+//*************************
+
+public class GunManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
